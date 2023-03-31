c694bb04748e49ce72c4af5fb26719dfa56e213a
Ngo Tan Thieu <ngotanthieu369@gmail.com>
2023-03-31 18:15:41 +0700

    Modify services chrono parse date return with timezone vn

--- services/chrono.service.js
+++ services/chrono.service.js
@@ -1,14 +1,13 @@
 const chrono = require('chrono-node');
-const { parseISO } = require('./dayjs.service');
+const { parseISO, parseTimezone } = require('./dayjs.service');
 module.exports = {
     getAlarmByChrono : (text,timeNow,callback) =>{
         try {
             console.log(`Chrono ====>`)
-            const date = chrono.parseDate(text,{
-                instant: new Date(timeNow), 
-                timezone: -420,
-            });
-            console.log({currenDate:new Date(timeNow)})
+            const date = chrono.parseDate(text);
+            // let localTime = parseTimezone(date);
+            // console.log({getAlarmByChrono:localTime.format("h:mm A DD/MM/YYYY")})
+            // console.log({timeNow})
             console.log({date});
             // console.log({parseISO:parseISO(date)})
             callback(parseISO(date),null)
--- services/dayjs.service.js
+++ services/dayjs.service.js
@@ -1,24 +1,44 @@
-const dayjs = require('dayjs')
-const getDiff =  (timeA,timeB) =>{
-    try {
-        let a = dayjs(timeA)
-        let b = dayjs(timeB)
-        return a.diff(b) 
-    } catch (error) {
-        console.log({error})
-    }
+const dayjs = require("dayjs");
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
+const timezoneName = 'Asia/Ho_Chi_Minh'
+const timezoneOffset = new Date().getTimezoneOffset()
+const timezoneConfig = {
+  name: timezoneName,
+  offset: timezoneOffset,
 }
 module.exports = {
-    parseISO : (text) =>{
-        try {
-            const outputFormat = 'h:mm A DD/MM/YYYY';
-            let parse = dayjs(text);
-            // console.log({timeNow,timeTaget:parse.valueOf()})
-            // let diff = getDiff(timeNow,parse.millisecond());
-            let diff = parse.unix() * 1000;
-            return {format:parse.format(outputFormat),day:parse.date(),month:parse.month(),year:parse.year(),hour:parse.hour(),minute:parse.minute(),second:parse.second(),diff}
-        } catch (error) {
-            console.log({error})
-        }
-    },
-}
+  parseISO: (text) => {
+    try {
+      const outputFormat = "h:mm A DD/MM/YYYY";
+      let parse = dayjs(text).utcOffset(timezoneOffset).tz(timezoneName);
+      // console.log({timeNow,timeTaget:parse.valueOf()})
+      console.log({parseISO:parse.format("h:mm A DD/MM/YYYY")})
+      // let diff = getDiff(timeNow,parse.millisecond());
+      let diff = parse.unix() * 1000;
+      return {
+        format: parse.format(outputFormat),
+        day: parse.date(),
+        month: parse.month(),
+        year: parse.year(),
+        hour: parse.hour(),
+        minute: parse.minute(),
+        second: parse.second(),
+        diff,
+      };
+    } catch (error) {
+      console.log({ error });
+    }
+  },
+  parseTimezone: (parsedDate) => {
+    try {
+    const localDate = dayjs(parsedDate).utcOffset(timezoneOffset).tz(timezoneName)
+      console.log({localDate:localDate.format("h:mm A DD/MM/YYYY")})
+      return localDate ;
+    } catch (error) {
+      console.log({ error });
+    }
+  },
+};
