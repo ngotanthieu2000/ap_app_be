16b23071bd2bcd9499a7822b8cc838f46004b8bd
Ngo Tan Thieu <ngotanthieu369@gmail.com>
2023-03-31 08:04:08 +0700

    Modify service dayjs update output timeout

--- services/dayjs.service.js
+++ services/dayjs.service.js
@@ -1,19 +1,22 @@
 const dayjs = require('dayjs')
+const getDiff =  (timeA,timeB) =>{
+    try {
+        let a = dayjs(timeA)
+        let b = dayjs(timeB)
+        return a.diff(b) 
+    } catch (error) {
+        console.log({error})
+    }
+}
 module.exports = {
     parseISO : (text,timeNow) =>{
         try {
             const outputFormat = 'h:mm A DD/MM/YYYY';
             let parse = dayjs(text);
-            return {format:parse.format(outputFormat),day:parse.date(),month:parse.month(),year:parse.year(),hour:parse.hour(),minute:parse.minute(),second:parse.second(),diff:getDiff(timeNow,parse)}
-        } catch (error) {
-            console.log({error})
-        }
-    },
-    getDiff: (timeA,timeB) =>{
-        try {
-            let a = dayjs(timeA)
-            let b = dbyjs(timeB)
-            return a.diff(b) 
+            // console.log({timeNow,timeTaget:parse.valueOf()})
+            // let diff = getDiff(timeNow,parse.millisecond());
+            let diff = parse.unix() * 1000;
+            return {format:parse.format(outputFormat),day:parse.date(),month:parse.month(),year:parse.year(),hour:parse.hour(),minute:parse.minute(),second:parse.second(),diff}
         } catch (error) {
             console.log({error})
         }
